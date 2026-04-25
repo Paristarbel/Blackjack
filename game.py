a2121fdf542daf1e6e75be3926b1eae25c855ba2
Paristarbell <nyoniparis37@gmail.com>
2026-04-25 08:26:30 +0000

    Fixing the conditions

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ def shuffle_cards():
     is_running=True
 
     while is_running:
-    
+        
         choice= input("Do you want to play a game of Black Jack (y or n)?")
         choice=choice.lower()
     
@@ -20,37 +20,54 @@ def shuffle_cards():
             computer=cards[2:4]
             print(cards)
             print(f"Your cards :{player}, current score {sum(player)}")
-            print(f"Computer's first card: {computer}")
+            print(f"Computer's first card: {[computer[0]]}")
 
             choose_card=input("Type 'y' to get another card,type 'n' to pass :").lower()
+
+            next_card=4
+
             if choose_card =='y':
                 #For player
-                for i in range(length):
-                    if i == 3:
-                        i+=1
-                        player.append(cards[i])
-                        print(f"Your final hand :{player} , final score {sum(player)}")
-                #Computer 
-                sum_computer=sum(computer)
-                for i in range(len(computer)):
-                    if i == 3 :
-                        continue
-                    elif i==4:
-                        i+=1
-                        computer.append(cards[i])
-                        print(f"Computer's hand :{computer} , final score {sum(computer)}")
-                
+
+                if sum(player)==21 and len(player)==2:
+                    return 0
+                if 11 in player and sum(player)>=21:
+                    return 0
+           
+                player.append(cards[next_card])
+                next_card+=1
+                print(f"Your final hand :{player} , final score {sum(player)}")
+
+                #For Computer
+                if sum(computer)==21 and len(computer)==2:
+                    return 0
+                if 11 in computer and sum(computer)>=21:
+                    computer.remove(11)
+                    computer.append(1)
+                computer.append(cards[next_card])
+                next_card+=1
+                print(f"Computer final hand :{computer} , final score {sum(computer)}")
+
                 
             if choose_card =="n":
-                print(f"Your final hand :{player} , final score {sum(player)}")
-                sum_computer=sum(computer)
-                for i in range(len(cards)):
-                    if  i==3  :
-                        i+=1
-                        computer.append(cards[i])
-                print(f"Computer final hand :{computer} , final score {sum(computer)}")
-                
 
-                # if sum(player) < sum(computer):
-                #     print("Opponent went over ,you WIN🌟✊😭")
+            
+
+                # if no then you must leave your player like that ,print their previous value of cards 
+                print(f"Your final hand :{player} , final score {sum(player)}")
+
+
+                computer.append(cards[next_card])
+                next_card+=1
+                print(f"Computer final hand :{computer} , final score {sum(computer)}")
+                is_running=False
+
+    if sum(computer)>sum(player) :
+        print("You went over ,you  LOSE😞")
+
+    if sum(player) > sum(computer):
+        print("Opponent went over ,you WIN🌟✊😭")
+    if sum(player) == sum(computer):
+        print("Draww✊")
+
 shuffle_cards()
